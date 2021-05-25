34897fc744d9d76ff3f4b2193b06bc18c23b45b1
Reinaldo <reinaldo.filho@sdredes.com.br>
2021-05-25 11:44:02 -0300

    changing acceptance test

--- __tests__/AcceptanceTest.js
+++ __tests__/AcceptanceTest.js
@@ -1,14 +1,39 @@
-const webdriver = require('selenium-webdriver')
-//const port = Number(new URL(frontedURL).port)
-    
-describe("Test site", () => {
-    test("Verify site get", async () => {
-        let a = await getsite()
-        expect(a).not.toBe(false)
+const webdriver = require('selenium-webdriver');
+
+const capabilities = {
+    'browserName': 'chrome',
+};
+
+describe('webdriver', () => {
+    let driver;
+   
+    beforeAll(async () => {
+      driver = new webdriver.Builder()
+        .usingServer('http://10.0.1.10:4444')
+        .withCapabilities(capabilities)
+        .build();
+   
+      // eslint-disable-next-line no-undef
+      await driver.get(
+        `https://www.selenium.dev`,
+      );
+    }, 10000);
+   
+    afterAll(async () => {
+      await driver.quit();
+    }, 15000);
+
+    test('it performs a validation of the search box on the page', async () => {
+        await driver.get('https://www.selenium.dev')
+        const title = await driver.findElement(webdriver.By.tagName('h1')).getText()
+        expect(title).toContain("Selenium automates browsers. That's it!")
     })
 })
 
-async function getsite(){
+
+
+//const port = Number(new URL(frontedURL).port)
+/*async function getsite(){
     let driver = new webdriver.Builder()
     .forBrowser('chrome')
     .usingServer('http://localhost:4444')
@@ -25,3 +50,4 @@ async function getsite(){
         await driver.quit()
     }
 }
+*/
